1b1be566ab55c951057876076f7a76c2dad5bedf
Roni Miguel <ronimiguel@gmail.com>
2022-08-28 16:52:54 -0300

    add: metodo para escrever e clicar

--- src/test/java/core/DSL.java
+++ src/test/java/core/DSL.java
@@ -4,9 +4,24 @@ import static core.DriverFactory.getDriver;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
+
 public class DSL {
 	public void browser(String Url) {
 		getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		getDriver().get(Url);
 	}
+	
+
+	public void escrever(By elemento, String texto) {
+		getDriver().findElement(elemento).sendKeys(texto);
+	}
+
+	public void clicar(By elemento) {
+		getDriver().findElement(elemento).click();
+	}	
+	
+
+
+
 }
