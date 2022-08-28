043b4ef8f3dd0ba0287fa9ab556217c24883b93c
Roni Miguel <ronimiguel@gmail.com>
2022-08-28 16:35:50 -0300

    Driver Factory

---
+++ src/test/java/core/DriverFactory.java
@@ -0,0 +1,27 @@
+package core;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+
+	private DriverFactory() {
+	}
+
+	private static WebDriver driver;
+
+	public static WebDriver getDriver() {
+		if (driver == null) {
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+		}
+		return driver;
+	}
+
+	public static void killDriver() {
+		if (driver != null) {
+			driver.quit();
+			driver = null;
+		}
+	}
+}
