13ef8e3240a77d228a0fa953a6280f7011773210
Roni Miguel <ronimiguel@gmail.com>
2022-08-28 17:25:50 -0300

    add: dados para preencher Insurant Data

--- src/test/java/tdm/DadosFormulario.java
+++ src/test/java/tdm/DadosFormulario.java
@@ -9,4 +9,17 @@ public class DadosFormulario {
 	static final String listPrice = "39000";
 	static final String licenseNumber = "hith3r3";
 	static final String annualMilleage = "11000";
+	
+	static final String firstName = "Jose";
+	static final String lastName = "Maria";
+	static final String birthDate = "08/01/1990";
+	static final String male = "Male";
+	static final String female = "Female";
+	static final String streetAddress = "201 1st Avenue";
+	static final String country = "United States";
+	static final String zipcode = "11232";
+	static final String city = "New York";
+	static final String occupation = "Farmer";
+	static final String hobbies = "Skydiving";
+	static final String website = "https://josemaria.com";
 }
