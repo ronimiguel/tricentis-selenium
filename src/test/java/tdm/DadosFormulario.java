e5a919959171ee1c97b5e502132b1fea8f08a5fc
Roni Miguel <ronimiguel@gmail.com>
2022-08-28 17:07:42 -0300

    add: dados para preencher Vehicle Data

---
+++ src/test/java/tdm/DadosFormulario.java
@@ -0,0 +1,12 @@
+package tdm;
+
+public class DadosFormulario {
+	static final String Make = "Ford";
+	static final String enginePerformance = "2000";
+	static final String dateOfManufacture = "08/01/2022"; //criar data
+	static final String numberOfSeats = "5";
+	static final String fuel = "Gas";
+	static final String listPrice = "39000";
+	static final String licenseNumber = "hith3r3";
+	static final String annualMilleage = "11000";
+}
